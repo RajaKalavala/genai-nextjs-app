7de596d57d7b0a389a7065409c43fa8870a8da65
Raja Kalavala <45046033+RajaKalavala@users.noreply.github.com>
2024-06-27 22:09:38 +0530

    Explore Story Complete

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,21 @@
+import Image from 'next/image'
+import Logo from '@/images/logo1.jpg'
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+
 export default function Home() {
   return (
-    <main className="">
+    <main className="flex-1 flex flex-col">
+      <section className="flex-1 grid grid-cols-1 lg:grid-cols-2">
+        <div className="bg-purple-700 flex flex-col space-y-5 justify-center items-center order-1 lg:-order-1 pb-10">
+          <Image src={Logo} height={250} alt="logo" />
+
+          <Button asChild className="px-20 bg-purple-700 p-10 text-xl">
+            <Link href="/stories">Explore Story Library</Link>
+          </Button>
+        </div>
+      </section>
+
       <h1>AI AGent</h1>
     </main>
   )
